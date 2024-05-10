41148ef835dee68a276d8d36d81a6bb7b9f931ae
Florencia <florenciauehara@gmail.com>
2024-05-10 16:42:09 -0300

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,112 @@ const Navbar = () => {
     backgroundColor: 'rgba(37, 25, 43, 0.6)', 
     color: '#D9D9D9', 
     padding: '20px', 
+    fontSize: '1rem',
+  };
+
+  const menuLinkStyle = {
+    color: '#D9D9D9', 
+    fontFamily: 'Inder',
+    fontSize: '1.2rem',
+    marginRight: '40px',
+    textDecoration: 'none', 
+    position: 'relative', 
+  }; 
+
+  const underlineStyle = {
+    content: '""',
+    position: 'absolute',
+    bottom: '-4px',
+    left: '0',
+    width: '0',
+    height: '2px', 
+    backgroundColor: '#C48DEF', 
+    transition: 'width 0.3s ease-in-out', 
+  };
+
+  const handleMouseEnter = (e) => {
+    e.target.style.color = '#C48DEF'; 
+    e.target.querySelector('.underline').style.width = '30%'; 
+  };
+
+  const handleMouseLeave = (e) => {
+    e.target.style.color = '#D9D9D9'; 
+    const underline = e.target.querySelector('.underline');
+    if (underline) {
+      underline.style.width = '0'; 
+    }
+  };
+
+  return (
+    <div className={styles.customNavbarWrapper}>
+      <BootstrapNavbar expand="lg" expanded={expanded}>
+        <BootstrapNavbar.Brand href="/" className="custom-navbar-brand" style={{ fontSize: '2.2rem', fontWeight: 'bold', fontFamily: 'Kanit', color: '#D9D9D9', marginLeft: '40px' }}> FLO </BootstrapNavbar.Brand>
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" style={{ background: '#D9D9D9' }} onClick={() => setExpanded(!expanded)} />
+        <BootstrapNavbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ms-auto" style={{ marginRight: '20px' }}>
+          <Nav.Link 
+              href="/" 
+              className={`${styles.navLink} custom-nav-link`} 
+              style={{ ...menuLinkStyle, ...(expanded ? expandedMenuLinkStyle : {}) }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            > 
+              Inicio
+              <span className="underline" style={underlineStyle}></span> 
+            </Nav.Link>
+            <Nav.Link 
+              href="/about" 
+              className={`${styles.navLink} custom-nav-link`} 
+              style={{ ...menuLinkStyle, ...(expanded ? expandedMenuLinkStyle : {}) }} 
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              Acerca de mí
+              <span className="underline" style={underlineStyle}></span> 
+            </Nav.Link>
+            <Nav.Link 
+              href="/projects" 
+              className={`${styles.navLink} custom-nav-link`} 
+              style={{ ...menuLinkStyle, ...(expanded ? expandedMenuLinkStyle : {}) }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              Proyectos              
+              <span className="underline" style={underlineStyle}></span> 
+            </Nav.Link> 
+            <Nav.Link 
+              href="/contact" 
+              className={`${styles.navLink} custom-nav-link`} 
+              style={{ ...menuLinkStyle, ...(expanded ? expandedMenuLinkStyle : {}) }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              Contacto
+              <span className="underline" style={underlineStyle}></span> 
+            </Nav.Link>
+          </Nav>
+        </BootstrapNavbar.Collapse>
+      </BootstrapNavbar>
+    </div>
+  );
+};
+
+export default Navbar;
+
+
+
+/*
+import React, { useState } from "react";
+import styles from '../components/Navbar.module.css'; 
+import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap';
+
+const Navbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const expandedMenuLinkStyle = {
+    backgroundColor: 'rgba(37, 25, 43, 0.6)', 
+    color: '#D9D9D9', 
+    padding: '20px', 
     fontSize: '1.1em',
   };
 
